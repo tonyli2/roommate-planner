3a3585eff1fb649114245fe93f4f500dc754ea2f
Tony Li <dawner125@gmail.com>
2023-01-21 15:34:17 -0800

    Created separate files for each page

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>New changes</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ AssignedPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function AssignedScreen() {
+  return (
+    <View>
+      <Text>This is the TODO screen</Text>
+    </View>
+  );
+}
+export default MyScreen;

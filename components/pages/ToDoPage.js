b121cc53717f3d1925e978e73d3978908130e677
Tony Li <dawner125@gmail.com>
2023-01-21 21:28:01 -0800

    Intermediate step for creating 'add task' button

--- components/pages/ToDoPage.js
+++ components/pages/ToDoPage.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, AsyncStorage } from 'react-native';
 
 function ToDoScreen({navigation}) {
+
+  //A Component which is a button which once pressed will create a new task
+  const TaskAdder = () => {
+
+    const [tasks, setTasks] = useState([]);
+
+    const handleAddTask = () => {
+        //setTasks([...tasks, ]);
+    };
+
+    return (
+        <View>
+            <Button title="Add Task" onPress={handleAddTask} />
+            {tasks}
+        </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>This is the TODO screen</Text>
-      <Button 
-            title="Assigned Tasks"
-            onPress={() => navigation.navigate("AssignedScreen")}
-        />
+    <>
+      <View>
         <Button 
-            title="Finished Tasks"
-            onPress={() => navigation.navigate("FinishedScreen")}
-        />
-    </View>
+              title="Add task"
+              onPress={() => navigation.navigate("FinishedScreen")}
+          />
+        <Text>This is the TODO screen</Text>
+        <Button 
+              title="Assigned Tasks"
+              onPress={() => navigation.navigate("AssignedScreen")}
+          />
+          <Button 
+              title="Finished Tasks"
+              onPress={() => navigation.navigate("FinishedScreen")}
+          />
+      </View>
+    </>
   );
 }
 export default ToDoScreen;

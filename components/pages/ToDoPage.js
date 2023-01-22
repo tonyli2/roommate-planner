5e157707ebb482f2768944fec0ee43b4b400db09
FLaMe <672769869@qq.com>
2023-01-21 22:24:42 -0800

    Updated task and todo

--- components/pages/ToDoPage.js
+++ components/pages/ToDoPage.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import Tasks from '../tasks/task';
 
 function ToDoScreen({navigation}) {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>This is the TODO screen</Text>
+
+      {/* Main module starts */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>TODO tasks</Text>
+
+        <View style={styles.items}>
+          {/* Add tasks here */}
+          <Tasks text={'Task 1'}/>
+          <Tasks text={'Task 2'}/>
+        </View>
+      </View>
+
       <Button 
             title="Assigned Tasks"
             onPress={() => navigation.navigate("AssignedScreen")}
@@ -16,4 +29,23 @@ function ToDoScreen({navigation}) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#A686BF',
+  },
+  taskWrapper: {
+    paddingTop: 20,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: '24',
+    fontWeight: 'bold',
+  },
+  items: {
+
+  },
+});
+
 export default ToDoScreen;
--- components/tasks/task.js
+++ components/tasks/task.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native';
 
-const Tasks = (content) => {
+const Tasks = (content, name) => {
 
     return (
         <View>
             <Text>{content.text}</Text>
+            <Text>Assignee: {name.text}</Text>
         </View>
     )
 }
